d84e6c9d33554b28736267ee48d785dc212b88d4
André Nechio <kgsnechio@gmail.com>
2020-07-20 20:52:54 -0300

    começo

---
+++ orgCodigo.js
@@ -0,0 +1,21 @@
+console.log('Sentença')  //Uma sentença é uma linha de código
+console.log(
+    'Isso tbm é uma sentença!'    //uma sentença tbm pode ocupar mais do que uma linha
+)
+
+{ } //Estrutura de um Bloco
+
+
+{       // Um bloco é composto por varias sentenças
+    console.log('Olá')  
+    console.log('Mundo!')
+}
+
+        //Tbm podemos ter blocos dentro de blocos
+{
+    {      
+        console.log(
+            'Bloco'
+        )
+    }
+}
