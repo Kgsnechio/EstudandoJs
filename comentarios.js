f2bbe77e8bd611e794e6890b5646ede0768c208a
André Nechio <kgsnechio@gmail.com>
2020-07-20 22:43:34 -0300

    comentarios e variaveis

---
+++ comentarios.js
@@ -0,0 +1,27 @@
+// Comentarios de uma linha
+
+console.log('SENTENÇA') //Geralmente usado para representar uma senteça
+
+/*
+Comentario 
+de 
+multiplas
+linhas
+*/
+
+/*
+* outra forma é por um asteristico por linha
+* mas não diferencia em nada, só estetica mesmo
+*
+* console.log('Isso é um comentario')
+*
+*/
+
+// transforme o codigo em comentario => selecione =>     ctrl  +  ; 
+
+// => Evite comentarios DESNECESSARIOS  <=
+
+/*  
+    Deixe sempre seu codigo de forma legivel
+    usando nomes que realmente tenha associação com o contexto
+*/
---
+++ varEconst.js
@@ -0,0 +1,20 @@
+var a = 3  // var-iavel   "Identificador"  = recebe =   $Valor
+
+let b = 4  // let tbm declara uma variavel
+
+var a = 30
+// let b = 40   let não pode ser redeclarada [ERRO!]
+b = 40
+
+console.log(a,b)
+
+a = 300       //podemos manipular livremente
+b = 400 
+
+console.log(a,b)
+
+const c = 5   //declara uma constante
+
+// c = 50   constante não pode ser alterada [ERRO!]
+
+console.log(c)
