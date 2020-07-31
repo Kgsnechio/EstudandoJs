c56037a175e712a656c49c54d31f712cea1d6ef8
André Nechio <kgsnechio@gmail.com>
2020-07-30 22:56:59 -0300

    declaração de variaveis

---
+++ usandoVar1.js
@@ -0,0 +1,14 @@
+{{{{ 
+    var sera = 'Será???'
+    console.log(sera)
+}}}}
+
+console.log(sera) // visivel em toda app
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+//console.log(local) //visivel somente dentro da function
